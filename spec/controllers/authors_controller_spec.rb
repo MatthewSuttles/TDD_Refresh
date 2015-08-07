8eb73468c46fe86327baa723cd4c8b315ce86136
MatthewSuttles <msuttles@accruent.com>
2015-08-07 14:58:37 -0500

    added show action

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AuthorsController, type: :controller do
 
-  describe "GET index" do
+  describe "GET #index" do
     it "assigns the authors object to @authors variable" do
       author1 = Fabricate(:author)
       author2 = Fabricate(:author)
@@ -13,6 +13,20 @@ RSpec.describe AuthorsController, type: :controller do
     it "renders the index template" do
       get :index
       expect(response).to render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    let(:author){author = Fabricate(:author)}
+
+    it "find author with given id and assign to @author variable" do
+      get :show, id: author
+      expect(assigns(:author)).to eq(author)
+    end
+
+    it "renders the show template" do
+      get :show, id: author
+      expect(response).to render_template :show
     end
   end
 
